57cd71f05766efc1eadf985b1c3dfbff09628066
Hajar <hajar2000alfarsi@gmail.com>
2026-04-09 18:59:07 +0400

    modification of task12

--- src/task12.java
+++ src/task12.java
@@ -17,11 +17,11 @@ public class task12 {
         String[] result = new String[end - start];
         for (int i = start; i < end; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                result[i-start] = "Fizz";
-            } else if (i % 3 == 0) {
-                result[i-start] = "Buzz";
-            } else if (i % 5 == 0) {
                 result[i-start] = "FizzBuzz";
+            } else if (i % 3 == 0) {
+                result[i-start] = "Fizz";
+            } else if (i % 5 == 0) {
+                result[i-start] = "Buzz";
             } else {
                 result[i-start] = String.valueOf(i);
             }

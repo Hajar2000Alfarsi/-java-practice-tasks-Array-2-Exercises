a3871c5c5ea03579d9c811ebf5712666e0d93d0d
Hajar <hajar2000alfarsi@gmail.com>
2026-04-09 18:38:20 +0400

    answer of task12

---
+++ src/task12.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class task12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter from where to start: ");
+        int start = sc.nextInt();
+        System.out.println("Enter Enter from where to end:");
+        int end = sc.nextInt();
+
+        task12 ex12 = new task12();
+        System.out.println("post 4 : " + Arrays.toString(ex12.fizzBuzz(start,end)));
+    }
+
+    public String[] fizzBuzz(int start, int end) {
+        String[] result = new String[end - start];
+        for (int i = start; i < end; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result[i-start] = "Fizz";
+            } else if (i % 3 == 0) {
+                result[i-start] = "Buzz";
+            } else if (i % 5 == 0) {
+                result[i-start] = "FizzBuzz";
+            } else {
+                result[i-start] = String.valueOf(i);
+            }
+        }
+        return result;
+    }
+}
